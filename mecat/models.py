1596dd7f6df70187043f05ef2280c30fbf8d2ed4
Von <von@von-XPS-630i.(none)>
2012-02-14 17:00:29 +1100

    Added Sample and DatasetWrapper models

---
+++ mecat/models.py
@@ -0,0 +1,23 @@
+from tardis.tardis_portal.models import *
+
+
+class Sample(models.Model):
+    experiment = models.ForeignKey(Experiment)
+    description = models.TextField(blank=True)
+    immutable = models.BooleanField(default=False)
+    objects = OracleSafeManager()
+
+    def __unicode__(self):
+        return self.description
+
+
+class DatasetWrapper(models.Model):
+    sample = models.ForeignKey(Sample)
+    dataset = models.ForeignKey(Dataset)
+    objects = OracleSafeManager()
+
+    def __unicode__(self):
+       return 'wrapper for' + dataset.description
+
+
+
