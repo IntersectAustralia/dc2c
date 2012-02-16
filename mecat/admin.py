ff5d0821e9e420b838aaa4b99b48ab6c021d896a
veronica <veronica@intersect.org.au>
2012-02-16 12:58:21 +1100

    Added DatasetWrapper and Sample models to admin interface

--- mecat/admin.py
+++ mecat/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.forms import TextInput
-from mecat.models import Sample
+from mecat.models import Sample, DatasetWrapper
 import django.db
 
 class SampleAdmin(admin.ModelAdmin):
     search_fields = ['description', 'experiment__id']
     
+class DatasetWrapperAdmin(admin.ModelAdmin):
+    search_fields = ['dataset__id', 'sample__id']
+    
 admin.site.register(Sample, SampleAdmin)
+admin.site.register(DatasetWrapper, SampleAdmin)
