7b02e56a9c0b0a108ed18dac35d545fe00ff6eba
veronica <veronica@intersect.org.au>
2012-03-23 11:46:59 +1100

    Fix up max length for field

--- mecat/forms.py
+++ mecat/forms.py
@@ -4,9 +4,9 @@ from django.forms.widgets import Textarea
 class SampleForm(forms.Form):
     name = forms.CharField(max_length=40, required=True)
     description = forms.CharField(required=True, widget=Textarea)
-    forcode_1 = forms.CharField(max_length=40, required=False, initial="060112 Structural Biology", widget=forms.TextInput(attrs={'class':'sample_forcode'}))
-    forcode_2 = forms.CharField(max_length=40, required=False, initial="060199 Biochemistry and cell Biology not elsewhere classified", widget=forms.TextInput(attrs={'class':'sample_forcode'}))
-    forcode_3 = forms.CharField(max_length=40, required=False, widget=forms.TextInput(attrs={'class':'sample_forcode'}))
+    forcode_1 = forms.CharField(max_length=100, required=False, initial="060112 Structural Biology", widget=forms.TextInput(attrs={'class':'sample_forcode'}))
+    forcode_2 = forms.CharField(max_length=100, required=False, initial="060199 Biochemistry and cell Biology not elsewhere classified", widget=forms.TextInput(attrs={'class':'sample_forcode'}))
+    forcode_3 = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class':'sample_forcode'}))
     notes = forms.CharField(required=False, widget=Textarea)
     
 
