93014e6c561223baf2f9f64c1bcfe333fd8e1a0a
veronica <veronica@intersect.org.au>
2012-04-04 13:18:34 +1000

    Removing experiment form handler and experiment wrapper model

--- mecat/models.py
+++ mecat/models.py
@@ -1,19 +1,25 @@
 from tardis.tardis_portal.models import *
 
-class ExperimentWrapper(models.Model):
-    experiment = models.ForeignKey(Experiment)
-    forcode1 = models.TextField(blank=True)
-    forcode2 = models.TextField(blank=True)
-    forcode3 = models.TextField(blank=True)
-    funded_by = models.TextField(blank=True)
-    notes = models.CharField(blank=True, max_length=100)
-    immutable = models.BooleanField(default=False)
-    objects = OracleSafeManager()
-
-    def __unicode__(self):
-        return 'wrapper for ' + self.experiment.description
+class Project(Experiment):
+    FORCODE1_DEFAULT = "060112 Structural Biology"
+    FORCODE2_DEFAULT = "060199 Biochemistry and cell Biology not elsewhere classified"
+    FUNDED_BY_CHOICES = [("Australian Research Council (ARC)", "Australian Research Council (ARC)"),
+                 ("Medical Research Council (NHMRC)", "Medical Research Council (NHMRC)")]
     
+    forcode1 = models.CharField(max_length=100, blank=True, default=FORCODE1_DEFAULT)
+    forcode2 = models.CharField(max_length=100, blank=True, default=FORCODE2_DEFAULT)
+    forcode3 = models.CharField(max_length=100, blank=True)
+    funded_by = models.CharField(max_length=100, blank=True, choices=FUNDED_BY_CHOICES)
+    notes = models.TextField(blank=True)
+
 class Sample(models.Model):
+    """
+    This Sample class is the 2nd tier in the DC2C 4-tier model:
+    Experiment -> Sample -> Dataset -> Datafile
+    
+    It points to the Experiment it belongs to, and it also has the 
+    rif-cs information for Sample.
+    """
     experiment = models.ForeignKey(Experiment)
     description = models.TextField(blank=False)
     name = models.CharField(max_length=100, blank=False)
@@ -29,6 +35,17 @@ class Sample(models.Model):
 
 
 class DatasetWrapper(models.Model):
+    """
+    This DatasetWrapper class serves as a wrapper to the existing Dataset
+    model in the core MyTARDIS. This is so we can add another middle tier to
+    the existing 3-tier MyTARDIS core model.
+    
+    It points to the Sample it belongs to, and the Dataset instance that it
+    is wrapping.
+    
+    In DC2C, when creating a Dataset in a Sample, we must create a DatasetWrapper
+    alongside the Dataset, so that we have the link between Sample and Dataset. 
+    """
     sample = models.ForeignKey(Sample)
     description = models.TextField(blank=False) # temporary
     dataset = models.ForeignKey(Dataset)
